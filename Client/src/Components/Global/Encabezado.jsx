a7c65fa8682e0a3d198a5248e163608ea1c1487d
Santiago Alvarez <santiaguitoalvarez03@gmail.com>
2025-10-21 15:23:03 -0300

    register

--- Client/src/Components/Global/Encabezado.jsx
+++ Client/src/Components/Global/Encabezado.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
 import { AiFillOpenAI } from "react-icons/ai";
+import axios from "axios"
 
 function Encabezado({ onNavigate, currentPage }) {
   const handleNavigation = (page) => {
     onNavigate(page)
+  }
+
+  const[user,Setuser]=useState('')
+  const[password,Setpassword]=useState('')
+  const[name,Setname]=useState('')
+
+  const[mensaje,Setmensaje]=useState()
+  const Registrosubmit=async(e)=>{
+    e.preventDefault
+    Setmensaje('')
+
+    try{
+      const Server=await axios.post('http://localhost:3000/api/registrarusuario',{
+        user,
+        password,
+        name
+
+      })
+      Setmensaje(Server.data.mensaje)||'registrado'
+      Setname('')
+      Setuser('')
+      Setpassword('')
+    }
+    catch(error){
+      console.error(error)
+    }
+
   }
 
   return (
@@ -39,6 +68,27 @@ function Encabezado({ onNavigate, currentPage }) {
             </button>
         </nav>
     </header>
+
+    <form action="" onSubmit={Registrosubmit}>
+    <h1>Registro de usuario</h1>
+    
+    <label htmlFor="usuario:"></label>
+    <input type="text" name="user" id="user" required
+    value={user} onChange={Setuser(e=>e.target.value)}/>
+
+
+    <label htmlFor="contraseña:"></label>
+    <input type="password" name="password" id="password" required
+    value={password} onChange={Setpassword(e=>e.target.value)}/>
+
+
+    <label htmlFor="nombre::"></label>
+    <input type="text" name="name" id="name" required
+    value={name} onChange={Setname(e=>e.target.value)}/>
+
+    <input type="submit" value="registrar" />
+    </form>
+    {mensaje && <h1>{mensaje}</h1>}
     </>
   )
 }
